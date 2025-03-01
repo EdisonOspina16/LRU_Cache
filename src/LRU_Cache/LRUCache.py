e6ed0d21e00be4d99ba2692088348a349c915550
Edison <edison100ospina@gmail.com>
2025-03-01 11:57:49 -0500

    Initial commit

---
+++ src/LRU_Cache/LRUCache.py
@@ -0,0 +1,12 @@
+from DoubleLinkedList import DoublyLinkedList
+
+
+class LRUCache:
+    def __init__(self):
+        pass
+
+    def get(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
